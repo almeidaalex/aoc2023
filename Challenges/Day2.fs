af55e6e4398967d8b633a2aa845abd5e35b2544e
Alex Almeida <alex.almeida@bouvet.no>
2023-12-23 22:27:06 +0100

    feat(day2): simplify type for Revealed games and Configuration

--- Challenges/Day2.fs
+++ Challenges/Day2.fs
@@ -10,17 +10,21 @@ module Day2 =
     | Green
     | Other
 
-  type Set = {
-    Revealed: Map<Cube, byte>
-  }
+  type Configuration = Map<Cube, byte>
 
-  type Game = {
-    Id: uint16
-    Sets: Set list
-  }
+  type Game =
+    { Id: uint16; Revealed: Map<Cube, byte> list}
+    member this.IsImpossible(configuration: Configuration): bool =
+      this.Revealed
+        |> List.exists (fun revelated ->
+          revelated |> Map.exists (fun cube amount ->
+            configuration |> Map.exists (fun configCube configAmount -> configCube = cube && amount > configAmount)))
+
+    member this.IsPossible(configuration: Configuration) =
+      not (this.IsImpossible configuration)
 
   let private getId (line: string) =
-    line.Substring(5,1) |> System.UInt16.Parse
+    line.Substring(5) |> System.UInt16.Parse
 
   let private mapColor (color: string) =
     match color with
@@ -30,16 +34,16 @@ module Day2 =
     | _ -> Other
 
   let private mapFromString (line: string) =
-    let parts = line.Trim().Split(' ')
+    let parts = line.Trim().Split(
+      ' ')
     let count = System.Byte.Parse(parts.[0])
     let color = mapColor parts.[1]
     (color, count)
 
-  let private getSetRevealed (line: string): Set =
+  let private getSetRevealed (line: string): Map<Cube, byte> =
     line.Split(',')
     |> Array.map mapFromString
     |> Map.ofArray
-    |> fun arr -> {Revealed = arr}
 
   let private mapSet (line: string) =
     line.Split(';')
@@ -49,5 +53,16 @@ module Day2 =
   let parseGame (lineGame: string): Game =
     let parts = lineGame.Split(':')
     let sets = parts.[1] |> mapSet
-    {Id = getId parts.[0]; Sets = sets}
+    {Id = getId parts.[0]; Revealed = sets}
 
+  let sumOfPossibleGames(games: string list, config: Configuration) : uint16 =
+    games
+    |> List.map parseGame
+    |> List.filter (fun g -> g.IsPossible(config))
+    |> List.sumBy (fun g -> g.Id)
+
+  let readFromFile =
+    let games = File.ReadAllLines("Files/day2_input.txt")
+              |> List.ofArray
+    let config = Configuration [( Red, 12uy); ( Green, 13uy); ( Blue, 14uy) ]
+    sumOfPossibleGames(games, config)
